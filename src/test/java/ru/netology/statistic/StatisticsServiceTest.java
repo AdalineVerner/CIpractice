ca29dee950558330ad207fccfe18ae4dbaa7e0a9
Xenia <xzinnatullina@mail.ru>
2023-07-25 21:20:56 +0300

    src modified

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-
-    void findMax(){
+    void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
