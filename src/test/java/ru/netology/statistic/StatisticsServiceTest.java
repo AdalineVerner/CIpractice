f58eb8af4923d90c4ca3a1342c544bd2661d99ab
Xenia <xzinnatullina@mail.ru>
2023-07-28 22:52:55 +0300

    jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,4 +15,15 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
